1bf81c965a82f8091a3319e03ee3504e7ca73b67
Ivan Ivanov <vankata384@gmail.com>
2023-02-12 18:08:42 +0200

    Added more solutions from arrays.

---
+++ 2. Programming/Arrays/Array processing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Array_processing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+                string commandName = command[0];
+
+                if (commandName == "Reverse") Array.Reverse(words);
+                else if (commandName == "Distinct") words = words.Distinct().ToArray();
+                else if (commandName == "Replace")
+                {
+                    int index = int.Parse(command[1]);
+                    string newValue = command[2];
+
+                    words[index] = newValue;
+                }
+            }
+            Console.WriteLine(string.Join(" ",words));
+        }
+    }
+}
---
+++ 2. Programming/Arrays/Set of the sums of the last K numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Set_of_the_sums_of_the_last_K_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            numbers[0] = 1;
+            numbers[1] = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                int sum = 0;
+                for (int i1 = 1; i1 <= Math.Min(i, k); i1++)
+                {
+                    sum += numbers[i - i1];
+                }
+                numbers[i] = sum;
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}

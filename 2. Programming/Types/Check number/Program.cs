1b9593dcad4037f0fa8a67e88825d274d41b1cea
vankata26 <vankata384@gmail.com>
2023-03-02 21:20:15 +0200

    Added some data types solutions

---
+++ 2. Programming/Types/Check number/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Check_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (long.TryParse(input, out _)) Console.WriteLine("integer");
+            else Console.WriteLine("floating-point");
+        }
+    }
+}
---
+++ 2. Programming/Types/Type limits/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Type_limits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine().ToLower();
+
+            if (type == "int")
+            {
+                Console.WriteLine(int.MaxValue);
+                Console.WriteLine(int.MinValue);
+            }
+            else if (type == "uint")
+            {
+                Console.WriteLine(uint.MaxValue);
+                Console.WriteLine(uint.MinValue);
+            }
+            else if (type == "byte")
+            {
+                Console.WriteLine(byte.MaxValue);
+                Console.WriteLine(byte.MinValue);
+            }
+            else if (type == "sbyte")
+            {
+                Console.WriteLine(sbyte.MaxValue);
+                Console.WriteLine(sbyte.MinValue);
+            }
+            else if (type == "long")
+            {
+                Console.WriteLine(long.MaxValue);
+                Console.WriteLine(long.MinValue);
+            }
+        }
+    }
+}
